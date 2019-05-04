94f3b238775a48abb453c11ad854612d4e353593
Henrique Couto <henrique.coutu@live.com>
2019-05-04 16:43:31 -0300

    sitemap added

---
+++ src/sitemap/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+  <Route>
+    <Route exact path="/" />
+    <Route exact path="/contact" />
+    {/* <Route exact path="/works" /> */}
+    <Route exact path="/skills" />
+  </Route>
+);
